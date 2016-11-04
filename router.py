afea741b048fe24cb4f2e20a9810efbb8c0897de
Ayaana Patel Sikora <AyaanaPS@users.noreply.github.com>
2016-11-03 20:00:15 -0700

    Update router.py

--- router.py
+++ router.py
@@ -1,1 +1,25 @@
+import packet.py
 
+class router:
+  
+  def __init__(self):
+    self.table = {}
+  
+  def computeShortestPath(router or host):
+  
+  def checkInRange(pDest, rangeDest):
+  
+  def chooseNextDest(self, newPacket):
+    pDest = newPacket.destination
+    for key in self.table:
+      found = checkInRange(pDest, key)
+      if(found):
+        nextDest = self.table[key]
+    
+    return nextDest
+    
+
+    
+
+    
+    
