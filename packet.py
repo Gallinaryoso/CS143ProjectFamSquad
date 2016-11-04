08720f8301e3c652269fc8827f8d667f4746d4d7
Ayaana Patel Sikora <AyaanaPS@users.noreply.github.com>
2016-11-03 19:45:15 -0700

    Update packet.py

--- packet.py
+++ packet.py
@@ -1,1 +1,12 @@
 
+class packet:
+  
+  packets = 0
+  
+  def __init__(self, source, destination, payload, acknowledgement):
+    self.source = source
+    self.destination = destination
+    self.payload = payload
+    self.acknowledgement = acknowledgement
+    packets += packets
+   
