5a24c64c9e2eb068d47fca42a6eb58595b4c4c9f
hlin3 <hlin3@caltech.edu>
2016-11-06 22:32:41 -0800

    Update link.py

--- link.py
+++ link.py
@@ -2,11 +2,11 @@ import router.py
 
 class link:
   
-  def __init__(self):
-    self.id
-    self.src
-    self.dest
-    self.rate
-    self.delay
-    self.buffer
+  def __init__(self, id, src, dest, rate, delay, buffer):
+    self.id = id
+    self.src = src
+    self.dest = dest
+    self.rate = rate
+    self.delay = delay
+    self.buffer = buffer
   
