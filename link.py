53e638c2b434963d59e1bf05fa05535eb9461391
Gallinaryoso <henryandrewbaer@gmail.com>
2016-11-07 01:10:52 -0800

    Update link.py

--- link.py
+++ link.py
@@ -2,10 +2,10 @@ import router.py
 
 class link:
   
-  def __init__(self, id, end1, end2, rate, delay, buffer):
+  def __init__(self, id, end_1, end_2, rate, delay, buffer):
     self.id = id
-    self.end1 = end1
-    self.end2 = end2
+    self.end_1 = end_1
+    self.end_2 = end_2
     self.rate = rate
     self.delay = delay
     self.buffer = buffer
