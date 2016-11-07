cd7b8d0c01a7c5cd91cfe8e219136702036df6ce
Gallinaryoso <henryandrewbaer@gmail.com>
2016-11-07 01:30:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import packet.py
 import link.py
 import router.py
 import flow.py
+import event_queue.py 
+
+def test_0():
+  event_queue = event_queue() 
+  host_1 = router(1, 1) 
+  host_2 = router(2, 1)
+  link_1 = link(1, host_1, host_2, 10, 10, 64) 
+  flow_1 = flow(host_1, host_2, 20, 1) 
+  
 
 def main():
   # Make a bunch of packets
