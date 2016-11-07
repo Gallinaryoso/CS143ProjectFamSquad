16e3ae8e547474243e362c5ed97b5afab7132aa3
Gallinaryoso <henryandrewbaer@gmail.com>
2016-11-07 00:54:09 -0800

    Update eventqueue.py

--- eventqueue.py
+++ eventqueue.py
@@ -1,1 +1,32 @@
+import heapq 
 
+class Event: 
+    def __init__(self, type, time):
+        '''holder - we need to figure out every possible event type
+        and how to deal with each specific case ''' 
+        self.type = type
+        self.time = time
+  
+class EventQueue: 
+    def __init__(self): 
+        self.queue = [] 
+        self.queue_size = 0 
+    
+    def insert_event(self, event): 
+        # event needs to be a (time, event) tuple to be sorted properly
+        heapq.heappush(self.queue, (event.time, event))  
+        self.queue_size += 1 
+    
+    def pop_event(): 
+        if self.queue == 0: 
+            #empty heap means we're done 
+            return -1 
+        popped = heapq.pop(self.queue)
+        self.queue_size -= 1
+        return popped 
+  
+   
+    
+           
+           
+           
