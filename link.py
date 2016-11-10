26a18c2a7f0191d2ca6349d6274c91bbba5d10a9
hlin3 <hlin3@caltech.edu>
2016-11-10 13:17:33 -0800

    Update link.py

--- link.py
+++ link.py
@@ -2,12 +2,12 @@ import router.py
 
 class link:
   
-  def __init__(self, id, end_1, end_2, capacity, rate, delay, buffer):
+  def __init__(self, id, end_1, end_2, capacity, delay, buffer):
     self.id = id
     self.end_1 = end_1
     self.end_2 = end_2
     self.capacity = capacity # in Mbps
-    self.rate = rate # in Mbps 
+    self.rate = 0 # in Mbps 
     self.delay = delay # in ms 
     self.buffer = buffer # in KB 
   
