85a70504a151f61800730a0114875b50513606ae
hlin3 <hlin3@caltech.edu>
2016-11-10 00:30:49 -0800

    Update packet.py

--- packet.py
+++ packet.py
@@ -1,10 +1,8 @@
 
 class packet:
   
-  def __init__(self, src, dest, payload=None, ack=0):
+  def __init__(self, src, dest, payload=None, ack=false):
     self.source = src
     self.destination = dest
     self.payload = payload
     self.acknowledgement = ack
-   
-
