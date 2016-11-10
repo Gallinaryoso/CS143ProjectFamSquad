9bef4a09beacfb209d791f5aa262345bcd735bfc
hlin3 <hlin3@caltech.edu>
2016-11-10 13:07:42 -0800

    Update link.py

--- link.py
+++ link.py
@@ -6,7 +6,7 @@ class link:
     self.id = id
     self.end_1 = end_1
     self.end_2 = end_2
-    self.capacity = capacity #in Mbps
+    self.capacity = capacity # in Mbps
     self.rate = rate # in Mbps 
     self.delay = delay # in ms 
     self.buffer = buffer # in KB 
