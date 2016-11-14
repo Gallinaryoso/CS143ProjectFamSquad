8c498a61b2a68f2e7243e10f24daf9e2bbdc4a54
hlin3 <hlin3@caltech.edu>
2016-11-14 15:09:22 -0800

    Update packet.py

--- packet.py
+++ packet.py
@@ -1,7 +1,8 @@
 
 class packet:
   
-  def __init__(self, src, dest, size):
+  def __init__(self, id, src, dest, size):
+    self.id = id
     self.source = src
     self.destination = dest
     self.size = size #bytes
