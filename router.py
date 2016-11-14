f1d6e4999fbe3f9ab76acbeb0c00f614bbe923b1
hlin3 <hlin3@caltech.edu>
2016-11-14 01:22:04 -0800

    Update router.py

--- router.py
+++ router.py
@@ -2,10 +2,9 @@ import packet.py
 
 class router:
   
-  def __init__(self, id, host_flag):
+  def __init__(self, id,):
     self.id = id
     self.table = {}
-    self.host_flag = host_flag
   
   def computeShortestPath(router, links):
   
