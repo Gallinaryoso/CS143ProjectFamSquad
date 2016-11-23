cd9a3210e53e090e63ec24ef67549d05b7ac0c16
hlin3 <hlin3@caltech.edu>
2016-11-23 13:23:29 -0800

    Update packet.py

--- packet.py
+++ packet.py
@@ -1,14 +1,14 @@
 
 class packet:
   
-  def __init__(self, id, src, dest, type, size):
+  def __init__(self, id, src, dest, type, size, current_router):
     self.id = id
     self.source = src
     self.destination = dest
     self.type = type #packet, ack, or route
     self.size = size #bytes
+    self.current_router = current_router
     self.delay = 0
     self.route = []
-    self.router = 0
-    self.left_source = -1
-    self.arrived_at_dest = -1
+    self.route_index = 0
+    #self.arrived_at_dest = -1
