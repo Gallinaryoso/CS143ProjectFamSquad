960d61bb62cc9f0f78a73715368603bff6aeac94
Ayaana Patel Sikora <AyaanaPS@users.noreply.github.com>
2016-11-22 18:51:46 -0800

    Update router.py

--- router.py
+++ router.py
@@ -7,7 +7,19 @@ class router:
     self.table = {}
   
   def computeShortestPath(self, links):
+  	numLinks = len(links)
+  	for link in links:
+  		bufCap = link.buffer_capacity
+
+
   	return -1
+  
+  def getNeighborLinks(self, links):
+  	neighbors = []
+  	for link in links:
+  		if(link.end_1 == self or link.end_2 == self):
+  			neighbors.append(link)
+  	return neighbors
   
   def chooseNextDest(self, newPacket):
     dest = newPacket.destination
