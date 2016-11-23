e36b4582ff4b646a21be9636c5a60a604d4c96e0
hlin3 <hlin3@caltech.edu>
2016-11-23 13:28:41 -0800

    Update packet.py

--- packet.py
+++ packet.py
@@ -2,13 +2,13 @@
 class packet:
   
   def __init__(self, id, src, dest, type, size, current_router):
-    self.id = id
-    self.source = src
-    self.destination = dest
-    self.type = type #packet, ack, or route
-    self.size = size #bytes
-    self.current_router = current_router
-    self.delay = 0
-    self.route = []
-    self.route_index = 0
+    self.id = id #ID of packet
+    self.source = src #source of packet
+    self.destination = dest #destination of packet
+    self.type = type #type as packet, ack, or route
+    self.size = size #packet size in bytes
+    self.current_router = current_router #current router of packet
+    self.delay = 0 #delay due to congestion from a particular link
+    self.route = [] #route that a packet has traveled already
+    self.route_index = 0 #index of route array where packet/ack is
     #self.arrived_at_dest = -1
