104e0a2493061042e6aa334346e4d2579dfbb8df
hlin3 <hlin3@caltech.edu>
2016-11-23 13:13:59 -0800

    Update flow.py

--- flow.py
+++ flow.py
@@ -2,7 +2,8 @@
 class flow:
   
   def __init__(self, src, dest, dataAMT, start):
-    self.src = src
-    self.dest = dest
+    self.src = src # source host of flow
+    self.dest = dest #destination host of flow
     self.amt = dataAMT # MB
     self.start = start # seconds
+    self.packets = [] # list of all packets needed in flow
