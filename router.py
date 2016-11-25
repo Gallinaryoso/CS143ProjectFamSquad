4c328482d527487af1c5cbc3821709ebfc6f1e8c
Ayaana <ayaanapatelsikora@gmail.com>
2016-11-25 13:43:37 -0800

    Working on shortest path

--- router.py
+++ router.py
@@ -6,21 +6,17 @@ class router:
     self.id = id
     self.table = {}
   
-  def computeShortestPath(self, links, event_queue):
-  	numLinks = len(links)
-  	for link in links:
-  		bufCap = link.buffer_capacity
-
-
-  	return -1
-  
-  def getNeighborLinks(self, links):
-  	neighbors = []
-  	for link in links:
-  		if(link.end_1 == self or link.end_2 == self):
-  			neighbors.append(link)
-  	return neighbors
-  
+  # This static method iterates through all the links and finds
+  # the ones that neighbor the inputted source.
+  @staticmethod
+  def getNeighborLinks(source, links):
+    neighbors = []
+    for link in links:
+      # If either end of the link is the inputted source, it is a neighbor.
+      if(link.end_2 == source or link.end_1 == source):
+        neighbors.append(link)
+    return neighbors
+    
   def chooseNextDest(self, newPacket):
     dest = newPacket.destination
     nextRouter = self.table[dest]
