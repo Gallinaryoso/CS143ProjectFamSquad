8e08109a37b6a6f71fc2034d55777ebee8b5f2b7
hlin3 <hlin3@caltech.edu>
2016-11-25 17:24:26 -0800

    Allowed multiple flows and variable window size

--- link.py
+++ link.py
@@ -1,6 +1,6 @@
 import router
 import packet
-import eventqueue
+from eventqueue import event_queue, event 
 
 class link:
   
@@ -12,12 +12,46 @@ class link:
     self.delay = delay # link propagation time in ms 
     self.buffer_capacity = buffer_capacity # buffer capacity in KB
     self.buffer_occupancy = 0 # buffer occupancy in bytes
-    self.buffer_elements = [] # list ot packets in the buffer
+    self.buffer_elements = [] # list of packets in the buffer
   
   #update link buffer by adding a packet, only if the buffer is not filled
-  def updateBuffer(self, event_queue, time, packet, flow):
+  def addToBuffer(self, event_queue, time, packet, flow):
+    
+    #check if the next packet wil not overfill the link buffer
     if self.buffer_occupancy + packet.size \
-        < self.buffer_capacity * 1000:
-          self.buffer_occupancy += packet.size
-          self.buffer_elements.append(packet)
-          event_queue.insert_event(eventqueue.event('Buffering', time, packet, self, flow))
+      < self.buffer_capacity * 1000:
+      
+        #add the packet to the buffer and update its occupancy and elements
+        self.buffer_occupancy += packet.size
+        self.buffer_elements.append(packet)
+        
+        #add the packet buffering event to the event queue
+        event_queue.insert_event(event('Buffering', time, packet, self, flow))
+        
+    #if the buffer is overfilled, then the packet is dropped
+    else:
+      flow.occupancy -= 1
+      
+  #get the transmission time for a packet based on the link rate
+  def getTransmission(self, packet):
+    return packet.size * 8 / (self.rate * 1000000.)  
+    
+  #update all of the packet delays on a bufer when the first packet is removed
+  def updateBufferPackets(self, packet):
+    
+    #get transmission time of link
+    transmission = self.getTransmission(packet)
+    
+    #whether there the next link packet is at another router
+    switch = self.buffer_elements[0].current_router != packet.current_router
+    
+    #remove the packet from the buffer, updating the buffer occupancy
+    self.buffer_elements.remove(packet)
+    self.buffer_occupancy -= packet.size
+    
+    #add packet delays, equal to the removed packet's propagation, if the  
+    #half-duplex link has the next packet going the opposite direction
+    for i in range(len(self.buffer_elements)):
+      self.buffer_elements[i].delay += transmission
+      if switch != 0:
+        self.buffer_elements[i].delay += self.delay / 1000.  
