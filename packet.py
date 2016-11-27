4cf3d83eebf704130f773ca9792da2475ea5f03c
hlin3 <hlin3@caltech.edu>
2016-11-27 13:06:42 -0800

    Fixed link queuing delay updates

--- packet.py
+++ packet.py
@@ -7,7 +7,7 @@ class packet:
     self.type = type #type as packet, ack, or route
     self.size = size #packet size in bytes
     self.current_router = current_router #current router of packet
-    self.delay = 0 #delay due to congestion from a particular link
+    self.current_time = -1 #time of a packet, after congestion
     self.route = [] #route that a packet has traveled already
     self.route_index = 0 #index of route array where packet/ack is
     self.start_time = -1 #when the packet started buffering in the flow
