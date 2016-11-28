bcfd036042d25b0cec4bc4081e76302ab5097291
hlin3 <hlin3@caltech.edu>
2016-11-27 16:50:41 -0800

    Update flow.py

--- flow.py
+++ flow.py
@@ -14,7 +14,7 @@ class flow:
     self.occupancy = 0 # how many packets are in the flow currently
     self.last_propagation = -1 # the time when the previous packet propagated
     self.flow_rate = 0 # the rate of packets going through the flow, in MB/s
-    self.window = 20 # the number of packets supposed to be flowing
+    self.window = 100 # the number of packets supposed to be flowing
     self.packet_delay = 0 # the time it took for a packet to flow, in ms
     self.flow_rate_history = [] # the flow rate over time
     self.window_history = [] # the window size over time
@@ -25,7 +25,6 @@ class flow:
     
     #get the number of packets needed based on the data amount
     packet_amount = (self.amt * 1000000) / data_packet_size
-    packet_amount = 500    
     
     #fill the packets of the flow, setting the current route as just the source
     self.packets = [0] * packet_amount
@@ -62,29 +61,42 @@ class flow:
         transmission = first_link.getTransmission(new_packet) 
         
         #if there is delay from other packets in the buffer, delay this packet
-        if len(first_link.buffer_elements) > 0 and time < \
-          first_link.buffer_elements[len(first_link.buffer_elements) - 1]\
-          .current_time:
+        if len(first_link.buffer_elements) > 0:
           
-          #set the current time of the new packet based on link delay
-          new_packet.current_time = first_link.buffer_elements \
-            [len(first_link.buffer_elements) - 1].current_time
+          #get the last packet on the first link's buffer
+          last = first_link.buffer_elements[len(first_link.buffer_elements) - 1]
           
-          #insert the buffering event for the new packet on the first link
-          event_queue.insert_event(event('Buffering', new_packet.current_time, \
-                                         new_packet, first_link, self))          
+          #check whether there is half-duplex congestion
+          switch = last.current_router != new_packet.current_router          
+          
+          #if there is no half-duplex congestion, add transmission accordingly
+          if switch == 0:
+            if time < last.current_time:
+              #set the current time of the new packet with link congestion
+              new_packet.current_time = last.current_time + transmission             
+            else:
+              #set the current time of the new packet without link congestion
+              new_packet.current_time = time + transmission
+            
+          #if there is half-duplex congestion, also add propagation accordingly
+          else:
+            if time < last.current_time:
+              #set the current time of the new packet with link congestion
+              new_packet.current_time = last.current_time + transmission + \
+                first_link.rate / 1000.
+            else:
+              #set the current time of the new packet without link congestion
+              new_packet.current_time = time + transmission
         else:
-          #set the current time of the new packet
           new_packet.current_time = time + transmission
           
-          #insert the buffering event for the new packet on the first link
-          event_queue.insert_event(event('Buffering', time + transmission,
-                                         new_packet, first_link, self))
+        #insert the buffering event for the new packet on the first link
+        event_queue.insert_event(event('Buffering', new_packet.current_time,
+                                       new_packet, first_link, self))
                   
         #append the packet to the link buffer and increase buffer occupancy
         first_link.buffer_occupancy += new_packet.size
         first_link.buffer_elements.append(new_packet)
-        
         
         #update the current packet (not in the flow) and increment occupancy
         self.current_packet += 1
