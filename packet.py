f1c40e53f86558a8bdddaa137bc54d4c2105d9b0
Ayaana <ayaanapatelsikora@gmail.com>
2016-11-28 18:01:06 -0800

    Added variables for dynamic routing

--- packet.py
+++ packet.py
@@ -4,10 +4,11 @@ class packet:
     self.id = id #ID of packet
     self.source = src #source of packet
     self.destination = dest #destination of packet
-    self.type = type #type as packet, ack, or route
+    self.type = type #type as packet, ack, or message
     self.size = size #packet size in bytes
     self.current_router = current_router #current router of packet
     self.current_time = -1 #time of a packet, after congestion
     self.route = [] #route that a packet has traveled already
     self.route_index = 0 #index of route array where packet/ack is
     self.start_time = -1 #when the packet started buffering in the flow
+    self.current_weight = 0 #keeps track of link weights encountered for message
