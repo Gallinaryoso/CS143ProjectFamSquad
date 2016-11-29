e557e5eb785b313d83913ab8b837a63e070a1e6f
Ayaana <ayaanapatelsikora@gmail.com>
2016-11-28 23:55:13 -0800

    Fixed router.py

--- router.py
+++ router.py
@@ -2,10 +2,7 @@ import packet
 
 class router:
 
-  # This is defined globally for all instances of the router class
-  # This is a dictionary of dictionaries for each host.
-  globalTable = {}
-  data_message_size = 64
+  data_message_size = 64 # This is the size of the message packet
 
   def __init__(self, id):
     self.id = id
@@ -19,7 +16,8 @@ class router:
   #Initialize new packet with this router as the destination
   def initializeRouterMessages(self, flows):
     packets = []
-    # For every host that exists, make a new packet 
+    # For every flow that exists, make a new packet with the flow src as the
+    # source for the packet and this router instance as the destination.
     for i in range(len(flows)):
       message = packet(i, flows[i].src, self, 'message', data_message_size, \
         flows[i].src)
