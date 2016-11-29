6035495f1b525375c49bdcec1e8a29bac90c4050
Ayaana <ayaanapatelsikora@gmail.com>
2016-11-29 02:51:37 -0800

    Fixed updateTable to work for dynamic routing

--- router.py
+++ router.py
@@ -8,25 +8,43 @@ class router:
     self.id = id
     self.table = {}
 
-  def updateTable(self, globalTable):
-    for host in globalTable:
-      routers = globalTable[host]
-      self.table[host] = routers[self]
+  def updateTable(self, packet):
+    host = packet.source
+
+    # If the host is already in the table, compare the weight in the table
+    # to the weight seen in this packet. If the new weight is less, replace
+    # the data.
+    if(host in self.table.keys()):
+      data = self.table[host]
+      if(data[0] < packet.current_weight):
+        self.table[host] = [packet.current_weight, \
+          packet.route[len(packet.route) - 2]]
+
+    # If the host is not already in the table, add this data to the table.
+    else:
+      self.table[host] = [packet.current_weight, \
+        packet.route[len(packet.route) - 2]]
 
   #Initialize new packet with this router as the destination
   def initializeRouterMessages(self, flows):
-    packets = []
+
     # For every flow that exists, make a new packet with the flow src as the
     # source for the packet and this router instance as the destination.
+    # This message is then added to the flows list of packets.
     for i in range(len(flows)):
       message = packet(i, flows[i].src, self, 'message', data_message_size, \
         flows[i].src)
       message.route.append(flows[i].src.id)
-      packets.append(message)    
+      flow.packets.append(message) 
 
   def chooseNextDest(self, newPacket):
     dest = newPacket.destination
-    nextRouter = self.table[dest]
+
+    # Get the data corresponding to this destination and choose the next router
+    # from the data
+    vals = self.table[dest]
+    nextRouter = vals[1]
+
     return nextRouter
     
 
