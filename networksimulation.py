01ff77587e64210923087c63cd382764670471a1
Henry Baer <henryandrewbaer@gmail.com>
2016-11-29 13:55:24 -0800

    u

--- networksimulation.py
+++ networksimulation.py
@@ -9,7 +9,7 @@ import sys
 def main(argv): 
 	if len(sys.argv) != 3: 
 		print "python ProjectSimulation.py (0,1,2) (0,1,2)"
-		print "1st arg: test number"
+		print "1st arg: 0 - test_0; 1 - test_1; 2 - test_2"
 		print "2nd arg: con_ctrl: 0 - none; 1 - TCP Reno; 2 - FAST"
 
 	else: 
