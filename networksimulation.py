309c0e86ba26a1f2aede8f9dd9a1ff34ee1d2e04
Ayaana Patel Sikora <AyaanaPS@users.noreply.github.com>
2016-12-06 23:27:26 -0800

    Update networksimulation.py

--- networksimulation.py
+++ networksimulation.py
@@ -11,8 +11,8 @@ def main(argv):
 	# Check command line parameters
 	if len(sys.argv) != 3: 
 		print "python ProjectSimulation.py (0,1,2) (0,1)"
-		print "2nd arg: con_ctrl: 0 - none; 1 - TCP Reno; 2 - FAST"
-		print "3rd arg: verbose: 0 - no; 1 - yes"
+		print "1st arg: con_ctrl: 0 - none; 1 - TCP Reno; 2 - FAST"
+		print "2nd arg: verbose: 0 - no; 1 - yes"
 		print('\n')
 		sys.exit()
 
